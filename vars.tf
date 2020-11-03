ca480f3bc699c71037c6ff600bee1536dd0fe88f
Jenny Wu <jiwu6498@uni.sydney.edu.au>
2020-11-03 12:15:04 +1100

    add vars.tf

---
+++ vars.tf
@@ -0,0 +1,32 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "udacity"
+}
+
+variable "environment"{
+  description = "The environment should be used for all resources in this example"
+  default = "test"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "Australia East" 
+}
+
+variable "username"{
+  default = "Your username"
+}
+
+variable "password"{
+  default= "your password"
+}
+
+variable "server_names"{
+  type = list
+  default = ["uat","int"]
+}
+
+variable "packerImageId"{
+  default = "/subscriptions/59ce2236-a139-4c59-91d8-bedbf055dbb9/resourceGroups/udacity-rg/providers/Microsoft.Compute/images/PackerImage"
+}
+
